e77b4a8f151e48ee5526af7b8525a70f94c85396
aspklmrs <aspklmrs@gmail.com>
2019-06-16 00:00:34 -0700

    first

---
+++ list.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int n[5];
+	for(int j=0;j<5;j++)
+	{
+		cout<<"Enter a Number";
+		cin>>n[j];
+	}
+	for(int j=0;j<5;j++)
+	{
+		cout<<"\t"<<n[j];
+}
+	return 0;
+	}
